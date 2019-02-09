f4e0b7fe8d848bf87fdc3e45a60822b0e9b16c94
Konstantinos Tsirkou <ktsirkou@gmail.com>
2019-02-09 21:16:01 +0100

    User service for handling user related requests

---
+++ src/Service/UserService.php
@@ -0,0 +1,52 @@
+<?php declare(strict_types=1);
+
+namespace kstirkou\OAT\Service;
+
+use kstirkou\OAT\Entity\User;
+use kstirkou\OAT\Entity\UserCollection;
+use kstirkou\OAT\Repository\AbstractUserRepository;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class UserService
+{
+    /**
+     * @var AbstractUserRepository
+     */
+
+    protected $userRepository;
+    /**
+     * UserService constructor.
+     *
+     * @param AbstractUserRepository $userRepository
+     */
+    public function __construct(AbstractUserRepository $userRepository){
+        $this->userRepository = $userRepository;
+    }
+    /**
+     * @param string $loginId
+     * @return User
+     *
+     * @throws NotFoundHttpException
+     */
+    public function getUser(string $loginId): User
+    {
+        $user = $this->userRepository->findBy($loginId);
+        if (!$user) {
+            throw new NotFoundHttpException('User with loginId '.$loginId.' does not exist!');
+        }
+
+        return $user;
+    }
+
+    /**
+     * @param int         $limit
+     * @param int         $offset
+     * @param string|null $name
+     *
+     * @return array
+     */
+    public function getUsers(int $limit, int $offset, string $name = null):array
+    {
+        return $this->userRepository->findAllBy($limit, $offset, $name);
+    }
+}
